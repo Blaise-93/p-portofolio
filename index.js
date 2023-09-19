38cb68114505d62184285623bbf1bbaca7b37f7c
Blaise <ejikemeblaise08@gmail.com>
2023-09-19 18:14:35 +0100

    created image  carousel

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
 const headerHamMenuCloseBtn = document.querySelector(
   '.header__main-ham-menu-close'
 )
+
 const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
 
 hamMenuBtn.addEventListener('click', () => {
@@ -36,3 +37,40 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+
+// CAROUSEL JS
+
+let slideCount = 1;
+
+slideDisplay(slideCount);
+
+function slideAll(num) {
+  slideDisplay(slideCount += num);
+}
+
+function recentSlide(num) {
+  slideDisplay(slideCount = num);
+}
+
+function slideDisplay(num) {
+    let i;
+    let slideEl =  document.getElementsByClassName("slide-content");
+    let boxEl =  document.getElementsByClassName("box");
+    if (num > slideEl.length) {
+          slideCount = 1 
+        }
+    if (num < 1) {
+        slideCount = slideEl.length
+        }
+        
+    for (i = 0; i < slideEl.length; i++) {
+      slideEl[i].style.display = "none";
+    }
+    for (i = 0; i < boxEl.length; i++) {
+      boxEl[i].className = boxEl[i].className.replace("active", "");
+    }
+    slideEl[slideCount - 1].style.display = "block";
+    boxEl[slideCount - 1].className += " active";
+}
+
